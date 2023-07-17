64452d7c2e0797b725622901d32c6800df823dfc
KaykSilva <kayksilva489@gmail.com>
2023-07-17 19:22:48 -0300

    changes

--- src/components/InfoCards/InfoCards.jsx
+++ src/components/InfoCards/InfoCards.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './InfoCards.css'
+import React from "react";
+import "./InfoCards.css";
 export const InfoCards = () => {
   return (
-    <div className='Container-cards'>
-        <div className='cards'>
-    
+    <div className="Container-cards">
+      <div className="cards">
+   
+        <div className="cards-info">
+          <img src="https://i.waifu.pics/Owt_E3B.jpg" />
+          <span>Em breve</span>
         </div>
-        
+
+        <div className="cards-info">
+          <img src="https://i.waifu.pics/xLoL-16.jpg" />
+          <span>Em breve</span>
+        </div>
+
+        <div className="cards-info">
+          <img src="https://i.waifu.pics/_laUTLb.jpg" />
+          <span>Em breve</span>
+        </div>
+
+        <div className="cards-info">
+          <img src="https://i.waifu.pics/Owt_E3B.jpg" />
+          <span>Em breve</span>
+        </div>
+
+        <div className="cards-info">
+          <img src="https://i.waifu.pics/xLoL-16.jpg" />
+          <span>Em breve</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
